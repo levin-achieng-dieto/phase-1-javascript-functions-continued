0ab79cdb278982c40b7e755fdffc29ff741e7423
levin-achieng-dieto <levin.dieto@student.moringaschool.com>
2022-08-16 23:22:04 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate"){
+    console.log(`This Saturday, I want to ${activity}!`);
+    return `This Saturday, I want to ${activity}!`
+}
+
+
+const mondayWork = function (activity = "go to the office"){
+    console.log(`This Monday, I will ${activity}.`);
+    return `This Monday, I will ${activity}.`
+}
+
+const wrapAdjective = function(outer = "*"){
+    const inner = function(work = "special"){
+        let result = `You are ${work}`
+        return result("*")
+    }
+
+}
